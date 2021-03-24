b7f7846b1af3349d233fa2fb04c84aadee53f0f5
ehlkara <ehlullah.fb@gamil.com>
2021-03-24 15:21:48 +0300

    Add project files.

---
+++ UdemyIdentityServer.API1/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyIdentityServer.API1.Models;
+
+namespace UdemyIdentityServer.API1.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // /api/products
+        [Authorize(Policy = "ReadProduct")]
+        [HttpGet]
+        public IActionResult GetProducts() {
+            var productList = new List<Product>() {
+                new Product { Id = 1, Name = "Kalem", Price = 200, Stock = 500 },
+                  new Product { Id = 2, Name = "Silgi", Price = 200, Stock = 500 },
+                  new Product { Id = 3, Name = "Defter", Price = 200, Stock = 500 },
+                  new Product { Id = 4, Name = "Kitap", Price = 200, Stock = 500 },
+                 new Product { Id = 5, Name = "Bant", Price = 200, Stock = 500 } 
+            };
+            return Ok(productList);
+        }
+
+        [Authorize(Policy = "UpdateOrCreate")]
+        public IActionResult UpdateProduct(int id)
+        {
+            return Ok($"id'si {id} olan product güncellenmiştir.");
+        }
+
+        [Authorize(Policy = "UpdateOrCreate")]
+        public IActionResult CreateProduct(Product product)
+        {
+            return Ok(product);
+        }
+    }
+}
---
+++ UdemyIdentityServer.API2/Controllers/PicturesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyIdentityServer.API2.Models;
+
+namespace UdemyIdentityServer.API2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        [Authorize]
+        public IActionResult GetPictures()
+        {
+            var pictures = new List<Picture>()
+            {
+                new Picture{Id=1,Name="Doğa resmi",Url="dogaresmi.jpg"},
+                new Picture{Id=2,Name="Fil resmi",Url="dogaresmi.jpg"},
+                new Picture{Id=3,Name="aslan resmi",Url="dogaresmi.jpg"},
+                new Picture{Id=4,Name="kedi resmi",Url="dogaresmi.jpg"},
+                new Picture{Id=5,Name="köpek resmi",Url="dogaresmi.jpg"},
+            };
+            return Ok(pictures);
+        }
+    }
+}
